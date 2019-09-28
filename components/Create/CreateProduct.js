8d9903e98fb96fc1f9a946bff928bb1d4f827174
Amol Jagtap <amoljagtap17@gmail.com>
2019-09-29 01:20:35 +0530

    NEXTJS9-MERN: Add Create Product component

---
+++ components/Create/CreateProduct.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+
+import {
+  Form,
+  Input,
+  TextArea,
+  Button,
+  Image,
+  Message,
+  Header,
+  Icon
+} from 'semantic-ui-react'
+
+const CreateProduct = () => {
+  const [product, setProduct] = useState({
+    name: '',
+    price: '',
+    media: '',
+    description: ''
+  })
+  const [mediaPreview, setMediaPreview] = useState('')
+
+  const handleChange = event => {
+    const { name, value, files } = event.target
+    if (name === 'media') {
+      setProduct(prevState => ({ ...prevState, media: files[0] }))
+      setMediaPreview(window.URL.createObjectURL(files[0]))
+    } else {
+      setProduct(prevState => ({ ...prevState, [name]: value }))
+    }
+    console.log('product', product)
+  }
+
+  return (
+    <>
+      <Header as="h2" block>
+        <Icon name="add" color="orange" />
+        Create New Product
+      </Header>
+      <Form>
+        <Form.Group widths="equal">
+          <Form.Field
+            control={Input}
+            name="name"
+            label="Name"
+            placeholder="Name"
+            onChange={handleChange}
+          />
+          <Form.Field
+            control={Input}
+            name="price"
+            label="Price"
+            placeholder="Price"
+            min="0.00"
+            step="0.01"
+            type="number"
+            onChange={handleChange}
+          />
+          <Form.Field
+            control={Input}
+            name="media"
+            type="file"
+            label="Media"
+            content="Select Image"
+            accept="image/*"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Image src={mediaPreview} rounded centered size="small" />
+        <Form.Field
+          control={TextArea}
+          name="description"
+          label="Description"
+          placeholder="Description"
+          onChange={handleChange}
+        />
+        <Form.Field
+          control={Button}
+          color="blue"
+          icon="pencil alternate"
+          content="Submit"
+          type="submit"
+        />
+      </Form>
+    </>
+  )
+}
+
+export default CreateProduct
---
+++ pages/create.js
@@ -0,0 +1,5 @@
+import CreateProduct from '../components/Create/CreateProduct'
+
+const CreatePage = () => <CreateProduct />
+
+export default CreatePage
