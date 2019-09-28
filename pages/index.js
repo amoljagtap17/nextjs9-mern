de6cbcc97e180655723af25549883887ceffed9d
Amol Jagtap <amoljagtap17@gmail.com>
2019-09-28 13:05:56 +0530

    NEXTJS9-MERN: Fetch data on the server using 'getInitialProps'

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import React, { useEffect } from 'react'
 import axios from '../config/axios'
 
-const Home = () => {
-  useEffect(() => {
-    getProducts()
-  }, [])
-
-  const getProducts = async () => {
-    const response = await axios.get('/products')
-    console.log('response', response.data)
-  }
+const Home = ({ products }) => {
+  console.log('products', products)
 
   return <div>home</div>
+}
+
+Home.getInitialProps = async () => {
+  // fetch data on server
+  const response = await axios.get('/products')
+  // return response data as an Object
+  return { products: response.data }
+  // this object will be merged with existing props
 }
 
 export default Home
