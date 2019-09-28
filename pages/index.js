ce33737992706b18999a746709ada7f4005237db
Amol Jagtap <amoljagtap17@gmail.com>
2019-09-28 12:54:18 +0530

    NEXTJS9-MERN: Add 'products' api and hook up with index page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
+import React, { useEffect } from 'react'
+import axios from '../config/axios'
+
 const Home = () => {
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  const getProducts = async () => {
+    const response = await axios.get('/products')
+    console.log('response', response.data)
+  }
+
   return <div>home</div>
 }
 
