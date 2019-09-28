4f6f0b9f9c75ded8d0c8861953c9a2b47dc62096
Amol Jagtap <amoljagtap17@gmail.com>
2019-09-28 12:29:41 +0530

    NEXTJS9-MERN: Add NProgress to App

--- components/App/Header.js
+++ components/App/Header.js
@@ -1,6 +1,12 @@
 import Link from 'next/link'
 import { Menu, Container, Image, Icon } from 'semantic-ui-react'
-import { useRouter } from 'next/router'
+import Router, { useRouter } from 'next/router'
+import NProgress from 'nprogress'
+
+NProgress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const user = false
 
--- components/App/Layout.js
+++ components/App/Layout.js
@@ -8,6 +8,7 @@ const Layout = ({ children }) => (
     <Head>
       {/* Stylesheets */}
       <link rel="stylesheet" type="text/css" href="/static/styles.css" />
+      <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
       <link
         rel="stylesheet"
         href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
